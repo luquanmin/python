6932a34af081600f07090b45b422d88fcd1244b8
luquanmin <luquanmin@163.com>
2022-02-22 22:48:52 +0800

    1.excise iloc and loc. 2.excise git rebase and commit 3.change the index of iloc to fragment to change its value

--- loc and iloc.py
+++ loc and iloc.py
@@ -4,13 +4,13 @@ import pandas as pd
 data=np.arange(0,30,2)
 data1=data.reshape(5,3)
 data2=pd.DataFrame(data1,columns=('a','b','c'))
-data2.iloc[2]
+data2.iloc[2]  #去指定单行多列，结果为series
+data2.iloc[1:4]
 
 
 
-
-print(data2.iloc[2])
-print(type(data2.iloc[2]))
-print(data2.iloc[2].shape)
+print(data2.iloc[1:4])
+print(type(data2.iloc[1:4]))
+print(data2.iloc[1:4].shape)
 
 
