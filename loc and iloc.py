4cdbc6b4d1c6bb83b6b1b70ef5623958e65e5746
luquanmin <luquanmin@163.com>
2022-02-23 06:16:57 +0800

    1.excise iloc and loc. 2.excise git rebase and commit 3.change the index of iloc to fragment to change its value 4，give more information to iloc[]

--- loc and iloc.py
+++ loc and iloc.py
@@ -1,16 +1,26 @@
 import numpy as np
 import pandas as pd
 
-data=np.arange(0,30,2)
-data1=data.reshape(5,3)
-data2=pd.DataFrame(data1,columns=('a','b','c'))
-data2.iloc[2]  #去指定单行多列，结果为series
-data2.iloc[1:4]
+data = np.arange(0, 30, 2)
+data1 = data.reshape(5, 3)
+data2 = pd.DataFrame(data1, columns=('a', 'b', 'c'))
 
+data2.iloc[2]  # 去指定单行多列，结果为series
 
+data2.iloc[1:4]  # 索引[开始，结束]，作弊右开
 
-print(data2.iloc[1:4])
-print(type(data2.iloc[1:4]))
-print(data2.iloc[1:4].shape)
+data2.iloc[2, 2]  # 指定行、列取到一个数字
 
+# data2.iloc[2,'c'] #不能直接取字段，报取值错误
 
+data2.iloc[2:3, 0:2]  # 可以用切片来指定行列，行左闭右开,列左闭右开
+
+data2.iloc[2:3, 0:].values  # 切片提取出来的行结果为DataFrame类型，将切片提取出来的数据转为数组
+
+data2.iloc[0:, 1]  # 指定列取的结果是series类型
+
+data2.iloc[0:, 0:1]  # 切片即使取一列的结果也是DataFrame类型
+
+print(data2.iloc[0:, 0:1])
+print(type(data2.iloc[0:, 0:1]))
+print(data2.iloc[0:, 0:1].shape)
