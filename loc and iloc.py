25fa17062789d6b19c01194e3afe9cbb747fc90a
luquanmin <luquanmin@163.com>
2022-02-27 12:35:05 +0800

    1.excise iloc and loc. 2.excise git rebase and commit 3.change the index of iloc to fragment to change its value 4，give more information to iloc[] 5、give loc example

--- loc and iloc.py
+++ loc and iloc.py
@@ -3,11 +3,11 @@ import pandas as pd
 
 data = np.arange(0, 30, 2)
 data1 = data.reshape(5, 3)
-data2 = pd.DataFrame(data1, columns=('a', 'b', 'c'))
+data2 = pd.DataFrame(data1, columns=['a', 'b', 'c'])
 
 data2.iloc[2]  # 去指定单行多列，结果为series
 
-data2.iloc[1:4]  # 索引[开始，结束]，作弊右开
+data2.iloc[1:4]  # 索引[开始，结束]，作闭右开
 
 data2.iloc[2, 2]  # 指定行、列取到一个数字
 
@@ -21,6 +21,19 @@ data2.iloc[0:, 1]  # 指定列取的结果是series类型
 
 data2.iloc[0:, 0:1]  # 切片即使取一列的结果也是DataFrame类型
 
-print(data2.iloc[0:, 0:1])
-print(type(data2.iloc[0:, 0:1]))
-print(data2.iloc[0:, 0:1].shape)
+# loc
+data2.loc[2]
+
+data2.loc[1:4]  # 左闭右闭区间
+
+# data2.loc[2,3]  #列不能取行，行不能取列
+
+data2.loc[2, 'c']  # 可以指定特定的行和列，得到指定的一个数字
+
+print(data2)
+
+print(data2.loc[2, 'c'])
+
+print(type(data2.loc[2, 'c']))
+
+print(data2.loc[2, 'c'].shape)
